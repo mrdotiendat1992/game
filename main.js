444536bd21d1864456cf97244c35ab6a5235f7d3
dotiendat <mrdotiendat1992>
2024-02-24 13:07:11 +0700

    init

---
+++ main.js
@@ -0,0 +1,9 @@
+function showDescription(element) {
+    var description = element.querySelector('.description');
+    description.classList.add('visible');
+}
+
+function hideDescription(element) {
+    var description = element.querySelector('.description');
+    description.classList.remove('visible');
+}
