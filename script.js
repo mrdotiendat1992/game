93716d01709e8776130841d7d01b0afb81ccb014
dotiendat <mrdotiendat1992>
2024-02-29 13:58:21 +0700

    init

--- script.js
+++ script.js
@@ -2,16 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch('game.json')
         .then(response => response.json())
         .then(data => {
-            const gameList = document.getElementById('gameList');
-            const gamesByTag = {};
-            data.forEach(game => {
-                game.tags.forEach(tag => {
-                    if (!gamesByTag[tag]) {
-                        gamesByTag[tag] = [];
-                    }
-                    gamesByTag[tag].push(game);
-                });
-            });
-        });
+            console.log(data)
+        })
     });
 
