8f854f7db9971a6dbe5584f15affcd9780213f60
dotiendat <mrdotiendat1992>
2024-02-29 14:41:36 +0700

    init

--- script.js
+++ script.js
@@ -11,6 +11,34 @@ document.addEventListener("DOMContentLoaded", function() {
                 tagLink.style.width = '100px';
 
                 gameList.appendChild(tagLink);
+
+                data.forEach(game => {
+                    if (game.listtag.includes(tag)) {
+                        const gameDiv = document.createElement('div');
+                        gameDiv.classList.add('game');
+
+                        const description = document.createElement('p');
+                        description.classList.add('description');
+                        description.textContent = game.description;
+
+                        const link = document.createElement('a');
+                        link.href = game.downloadLink;
+                        link.textContent = 'Tải về';
+
+                        gameDiv.appendChild(description);
+                        gameDiv.appendChild(link);
+
+                        // Thiết lập hình ảnh là background của div game
+                        const image = new Image();
+                        image.src = game.image;
+                        image.onload = function() {
+                            gameDiv.style.backgroundImage = `url('${game.image}')`;
+                        };
+
+                        gameList.appendChild(gameDiv);
+                    }
+                });
             });
         });
-    });
+});
+
