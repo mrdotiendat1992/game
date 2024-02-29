983dd9edeb6fe23ac1026874d7de486d90fd9b27
dotiendat <mrdotiendat1992>
2024-02-29 14:07:14 +0700

    init

--- script.js
+++ script.js
@@ -2,7 +2,44 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch('game.json')
         .then(response => response.json())
         .then(data => {
-            console.log(data)
-        })
-    });
+            const gameList = document.getElementById('gameList');
+            data.listtag.forEach(tag => {
+                const tagLink = document.createElement('a');
+                tagLink.href = `#${tag}`;
+                tagLink.textContent = tag;
+
+                const tagDiv = document.createElement('div');
+                tagDiv.classList.add('tag');
+                
+                tagDiv.appendChild(tagLink);
+
+                gameList.appendChild(tagDiv);
+            })
+        //     data.forEach(game => {
+        //         const gameDiv = document.createElement('div');
+        //         gameDiv.classList.add('game');
+
+        //         const description = document.createElement('p');
+        //         description.classList.add('description');
+        //         description.textContent = game.description;
+
+        //         const link = document.createElement('a');
+        //         link.href = game.downloadLink;
+        //         link.textContent = 'Tải về';
+
+        //         gameDiv.appendChild(description);
+        //         gameDiv.appendChild(link);
+
+        //         // Thiết lập hình ảnh là background của div game
+        //         const image = new Image();
+        //         image.src = game.image;
+        //         image.onload = function() {
+        //             gameDiv.style.backgroundImage = `url('${game.image}')`;
+        //         };
+
+        //         gameList.appendChild(gameDiv);
+        //     });
+        // })
+        .catch(error => console.error('Error fetching data:', error));
+});
 
